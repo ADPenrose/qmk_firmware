99542a61ff1ab7f80752c8ce4f91c4a9c130bdb6
Arturo Avila <mysticmetromx@gmail.com>
2023-03-04 13:47:59 -0600

    Added lighting layers and super alt+tab encoder macros.

--- keyboards/adpenrose/kintsugi/keymaps/oled_bongocat/keymap.c
+++ keyboards/adpenrose/kintsugi/keymaps/oled_bongocat/keymap.c
@@ -16,6 +16,10 @@
 #include QMK_KEYBOARD_H
 #include "animation_frames.h"
 
+bool is_alt_tab_active = false; // ADD this near the beginning of keymap.c
+uint16_t alt_tab_timer = 0;     // we will be using them soon.
+
+
 /* Base layout:
  * ,---------------------------------------------------------------------|
  * |`  |1  |2  |3  |4  |5  |6  |7  |8  |9  |0   |-   |=  |Backspace| OLED|
@@ -29,6 +33,38 @@
  * |Ctrl|GUI |Alt |     Space               |MO(1) |MO(2)|   |Lt |Dn |Rt |
  * `---------------------------------------------------------------------|'
  */
+
+ enum custom_keycodes {          // Make sure have the awesome keycode ready
+  ALT_TAB = SAFE_RANGE,
+  ALT_TAB_R,
+  ALT_TAB_L
+};
+
+// Light LEDs 1 to 16 green when caps lock is active. Hard to ignore!
+const rgblight_segment_t PROGMEM my_capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {1, 16, HSV_GREEN}       // Light all LEDs, starting with LED 1
+);
+// Light LEDs 1 to 16 in cyan when keyboard layer 1 is active
+const rgblight_segment_t PROGMEM my_layer1_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {1, 16, HSV_CYAN}
+);
+// Light LEDs 1 to 16 in purple when keyboard layer 2 is active
+const rgblight_segment_t PROGMEM my_layer2_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {1, 16, HSV_PURPLE}
+);
+// Light LEDs 1 to 16 in green when keyboard layer 3 is active
+const rgblight_segment_t PROGMEM my_layer3_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {1, 16, HSV_GOLD}
+);
+
+
+// Now define the array of layers. Later layers take precedence
+const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+    my_capslock_layer,
+    my_layer1_layer,    // Overrides caps lock layer
+    my_layer2_layer,    // Overrides other layers
+    my_layer3_layer     // Overrides other layers  
+);
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
@@ -49,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [2] = LAYOUT(
       _______,  _______,    _______,    _______,    _______,   _______,   _______,   _______,   _______,   _______,    _______,    _______,  _______, _______,
       _______,  _______,    _______,    _______,    _______,   _______,   _______,   _______,   _______,   _______,    _______,    _______,  _______, _______,
-      _______,  _______,    _______,    _______,    _______,   _______,   _______,   _______,   _______,   _______,    _______,    _______,  _______,            KC_MNXT,
+      _______,  _______,    _______,    _______,    _______,   _______,   _______,   _______,   _______,   _______,    _______,    _______,  _______,            ALT_TAB,
       _______,              _______,    _______,    _______,   _______,   _______,   _______,   _______,   _______,    _______,    _______,  _______, _______,   _______,
       _______,  _______,    _______,                           _______,                                    _______,    _______,              _______,  _______,  _______
     ),
@@ -67,7 +103,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [0] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
     [1] =  { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
-    [2] =  { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
+    [2] =  { ENCODER_CCW_CW(ALT_TAB_L, ALT_TAB_R) },
     [3] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) }
 };
 #endif
@@ -175,7 +211,59 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         }
     }
     #endif
-
+    /* Super Alt+Tab */
+    switch (keycode) { // This will do most of the grunt work with the keycodes.
+    case ALT_TAB:
+      if (record->event.pressed) {
+        if (!is_alt_tab_active) {
+          is_alt_tab_active = true;
+          register_code(KC_LALT);
+        }
+        alt_tab_timer = timer_read();
+        register_code(KC_TAB);
+      } else {
+        unregister_code(KC_TAB);
+      }
+        break;
+    case ALT_TAB_R:
+        if (record->event.pressed && is_alt_tab_active == true) {
+        tap_code(KC_RIGHT);
+        alt_tab_timer = timer_read();
+      }
+        break;
+    case ALT_TAB_L:
+        if (record->event.pressed && is_alt_tab_active == true) {
+        tap_code(KC_LEFT);
+        alt_tab_timer = timer_read();
+      }
+        break;
+  }
     return true;
 
+}
+
+void keyboard_post_init_user(void) {
+    // Enable the LED layers
+    rgblight_layers = my_rgb_layers;
+}
+
+bool led_update_user(led_t led_state) {
+    rgblight_set_layer_state(0, led_state.caps_lock);
+    return true;
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    rgblight_set_layer_state(1, layer_state_cmp(state, 1)); 
+    rgblight_set_layer_state(2, layer_state_cmp(state, 2));  
+    rgblight_set_layer_state(3, layer_state_cmp(state, 3));
+    return state;
+}
+
+void matrix_scan_user(void) { // The very important timer.
+  if (is_alt_tab_active) {
+    if (timer_elapsed(alt_tab_timer) > 1000) {
+      unregister_code(KC_LALT);
+      is_alt_tab_active = false;
+    }
+  }
 }
